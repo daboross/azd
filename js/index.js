177b72dec52c59289699e00736d9477a0ffa3742
Dabo Ross <daboross@daboross.net>
2014-03-05 09:16:52 -0800

    meh

--- js/index.js
+++ js/index.js
@@ -1,30 +1,26 @@
-function sendData() {
-    request.open("POST", "http://dabo.guru/py/notify", true);
-    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    request.send("HIIII");
-}
 function sendSuccess(data, textStatus, jqXHR) {
     var $value = $("#send-value");
-    $value.show()
+    $value.show();
     $value.html("Success!")
 }
 function sendFail(data, textStatus, jqXHR) {
     var $value = $("#send-value");
-    $value.show()
+    $value.show();
     $value.html("Failure: " + textStatus);
 }
 
 function sendData() {
-    request = $.ajax({
+    var sendForm = $("#send-form");
+    var request = $.ajax({
         url: "http://dabo.guru/py/notify",
         type: "POST",
-        data: $("#send-form").val(),
+        data: sendForm.val(),
         complete: sendSuccess,
         contentType: "text/plain;charset=UTF-8"
     });
     request.done(sendSuccess);
     request.fail(sendFail);
-    $("#send-form").hide();
+    sendForm.hide();
     $("#send-button").hide();
     var sendValue = $("#send-value");
     sendValue.show();
